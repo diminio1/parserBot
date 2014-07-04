7c801ac597fbd022880bd7f16a7906be475b5f32
leveretka <margoqueen95@gmail.com>
2014-07-04 18:36:02 +0300

    fixed bug with currency

--- Parsers/src/money/currency/Currency.java
+++ Parsers/src/money/currency/Currency.java
@@ -1,9 +1,12 @@
 package money.currency;
 
 import java.io.IOException;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+
+import banan.file.writer.BananFileWriter;
 
 public class Currency {
     
@@ -14,15 +17,17 @@ public class Currency {
         Document curDoc = null;
         try {
             curDoc = Jsoup.connect("http://finance.ua/ua/").timeout(100000).get();
-            Element currency = curDoc.select("table[class = table table-hover]").first();
+            Element currency = curDoc.select("table[class = table table-hover]").select("tbody").first();
             
-            dollar = Double.valueOf(currency.select("td").get(1).text());
+            dollar = Double.valueOf(currency.select("tr").first().select("td").get(2).ownText());
                 
-            euro = Double.valueOf(currency.select("td").get(3).text());
+            euro = Double.valueOf(currency.select("tr").get(1).select("td").get(2).ownText());
                 
         }
-        catch (IOException ex) {
-            ex.printStackTrace();
+        catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        	dollar = 12.00;
+        	euro = 16.48;
         }
     }
 }
